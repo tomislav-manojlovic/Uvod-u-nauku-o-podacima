91728aadc595500cce0e9645d8689ad9b15140c1
zarkovicj <zarkovicj20@gmail.com>
2025-10-25 01:53:50 +0200

    aaaaaaaaaaaa
    aaaaaaaaaaaaaa

--- seminarski.R
+++ seminarski.R
@@ -126,3 +126,175 @@ ggplot(data, aes(x = PoolArea, y = SalePrice)) + geom_point()
 
 values = c(935, 1299, 250, 314, 336, 707, 1397, 524, 692, 379, 1183, 
            186, 739, 598, 955, 636, 49, 810, 1062, 1191, 496, 198)
+
+data <- subset(data, !(Id %in% values))
+
+#missing_percent <- colSums(is.na(data)) / nrow(data) * 100
+#missing_percent[missing_percent > 0]
+
+# ----------------------------------------------------
+# NA VREDNOSTI
+
+sort(colSums(is.na(data))[colSums(is.na(data)) > 0])
+
+# Electrical - elektricni sistem
+# ima jednu NA
+# chr je, a treba da se pretvori u kat. TODO
+# posto nedostaje samo jedna vr, mozemo uzeti onu koja se najcesce javlja
+
+unique(data$Electrical)
+# View(data[is.na(data$Electrical),])
+
+# najcesce ponavljanja vrednost
+tbl <- xtabs(~Electrical, data = data)
+# najcesca vrednost je "SBrkr" - standardni prekidacii i Romex (tip kabla)
+most_common <- names(which.max(tbl))
+data$Electrical[is.na(data$Electrical)] <- most_common
+
+View(data)
+
+
+# MasVnrType - tip malterisanja
+# takodje chr -> kat.
+# takodje ima malo NA (osam), pa se moze iskoristiti najcesca vrednost
+str(data$MasVnrType)
+
+tbl <- xtabs(~MasVnrType, data = data)
+# najcesca vrednost je None
+most_common <- names(which.max(tbl))
+data$MasVnrType[is.na(data$MasVnrType)] <- most_common
+
+# povrsina zida koji je pokriven dekorativnim materijalom
+# numericka
+str(data$MasVnrArea)
+
+shapiro.test(data$MasVnrArea)
+# posto test ukazuje da raspodela nije normalna, koristimo medijanu
+ggplot(data, aes(x = MasVnrArea)) +
+  geom_histogram(binwidth = 50, fill = "#1f78b4", color = "black", alpha = 0.7) +
+  labs(
+    title = "Raspodela MasVnrArea",
+    x = "Povrsina",
+    y = "Broj kuca"
+  ) +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+  scale_x_continuous(breaks = seq(0, max(data$MasVnrArea, na.rm = TRUE), by = 50))
+
+data_median = median(data$MasVnrArea[!is.na(data$MasVnrArea)])
+# medijana je 0
+data$MasVnrArea[is.na(data$MasVnrArea)] <- data_median
+
+# BsmtQual - visina podruma 
+# kategorijska ORDINALNA - excellent (100+ in), good(90-99 in), ...
+# NA znaci da nema podruma, mozemo dodati novu kategoriju "NONE"
+# ima 37 NA vrednosti
+str(data$BsmtQual)
+
+data$BsmtQual[is.na(data$BsmtQual)] <- "NoBasement"
+
+# BsmtCond - stanje podruma
+# isto kao i BsmtQual kat. ORDINALNA
+# NA znaci da ne postoji podrum 
+str(data$BsmtCond)
+
+data$BsmtCond[is.na(data$BsmtCond)] <- "NoBasement"
+
+
+# BsmtFinType1 - kvalitet zavrsene povrsine podruma TODO PROVERI (unf ili no)
+# kategorijska ORDINALNA
+# isto kao prethodne 
+# NA znaci da ne postoji podrum
+str(data$BsmtFinType1)
+
+data$BsmtFinType1[is.na(data$BsmtFinType1)] <- "NoBasement"
+
+# Bsmt FinType2 - kvaliltet druge zavrsene povrsine podruma (ako postoji)
+# kategorijska ORDINALNA
+# isto kao prethodna 
+# NA znaci da ne postoji podrum
+str(data$BsmtFinType2)
+
+data$BsmtFinType2[is.na(data$BsmtFinType2)] <- "NoBasement"
+
+# BsmtExposure - da li podrum ima prozore ili vrata koja izlaze u dvoriste
+# kategorijska ORDINALNA
+# isto kao prethodna 
+# NA znaci da ne postoji podrum
+
+str(data$BsmtExposure)
+data$BsmtExposure[is.na(data$BsmtExposure)] <- "NoBasement"
+
+# GarageType, GarageYrBlt, GarageFinish, GarageQual, GarageCond
+# kategorijska ORDINALNA
+# NA znaci da ne postoji garaza
+
+View(data[which(is.na(data$GarageType)), ])
+
+data$GarageType[is.na(data$GarageType)] <- "NoGarage"
+data$GarageYrBlt[is.na(data$GarageYrBlt)] <- "NoGarage"
+data$GarageFinish[is.na(data$GarageFinish)] <- "NoGarage"
+data$GarageQual[is.na(data$GarageQual)] <- "NoGarage"
+data$GarageCond[is.na(data$GarageCond)] <- "NoGarage"
+
+
+# LotFrontage - duzina placa koja se granici sa ulicom
+# numericka
+# 254 NA
+
+# uradi NA kao da je 0 (ne granici se sa ulicom)
+str(data$LotFrontage)
+View(data[which(is.na(data$LotFrontage)), ])
+
+
+# FireplaceQu - kvalitet kamina
+# kat. Ordinalna 
+# NA - nema kamina
+
+data$FireplaceQu[is.na(data$FireplaceQu)] <- "NoFireplace"
+
+# Fence - kvalitet ograde
+# kat. nominalna
+# NA - nema ograde
+
+data$Fence[is.na(data$Fence)] <- "NoFence"
+
+# Alley - zadnji prilaz sa puta
+# kat. nominalna
+# NA - nema prilaz
+
+data$Alley[is.na(data$Alley)] <- "NoAccess"
+
+# MiscFeature - dodatni featuri
+# kat. nominalna
+# NA - nema dodatnih featura
+
+# MiscFeature nema poente posto postoji MiscVal
+# da li dropovati
+# MiscVal uvek tačno odražava postojanje/odsustvo MiscFeature
+View(data[is.na(data$MiscFeature), ])
+
+data$MiscFeature <- NULL
+
+# PoolQC - kvalitet bazena
+# kat. Ordinalna 
+# NA - nema bazena
+# postoji i poolArea; mozda dropovati PoolQC?
+
+data$PoolQC[is.na(data$PoolQC)] <- "NoPool"
+
+
+View(data)
+
+
+sort(colSums(is.na(data))[colSums(is.na(data)) > 0])
+
+
+
+#View(data[which(is.na(data$BsmtExposure)), ]) 
+
+# TODO test skup NA 
+
+data.test = read.csv("./data/test.csv")
+summary(data.test)
+
+summary(data)
